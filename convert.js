bd01059ac1de4f4bf26b4e2f892adc1f29c8fb66
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2025-08-23 11:36:06 +0200

    added test questions and lowered temperature

--- convert.js
+++ convert.js
@@ -1,8 +1,9 @@
-const cardData = require("./Standard.json")
+const cardData = require("./old-data/Standard.json")
 const fs = require("fs");
 
 const output = cardData.data.BIG.cards.map((card) => {
     return {
+      uuid: card.uuid,                            // unique id
       borderColor: card.borderColor,              // string like black, white, red
       colors: card.colors,                        // array with ["W", "R"] etc
       convertedManaCost: card.convertedManaCost,  // number
@@ -14,9 +15,11 @@ const output = cardData.data.BIG.cards.map((card) => {
       originalText: card.originalText,            // complete text information
       rarity: card.rarity,                        // string with common, mythic, etc...
       rulings: card.rulings,                      // array of objects with date and text of special ruling. BIG chunk of data but important
-      // type: card.type,                            // string for main type. needed?
-      types: card.types,                          // array of all types
+      types: card.types,                          // array of all types like artifact, creature, land, etc...
+      subtypes: card.subtypes,                    // for example, creature types like human, cleric, etc...
+      toughness: card.toughness,                  // string for toughness
+      power: card.power                           // string for power
     };
   });
 
-fs.writeFileSync("./output.json", JSON.stringify(output, null, 2), "utf8");
+fs.writeFileSync("./clean-standard-cards.json", JSON.stringify(output, null, 2), "utf8");
